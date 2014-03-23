680041bf47304a41c75629372893cfaeddfd251f
00willo <graham@williamsonsinc.id.au>
2014-03-23 23:00:25 +1100

    Added better error checking on connection

--- spacewalk-clean-old-packages.py
+++ spacewalk-clean-old-packages.py
@@ -30,14 +30,29 @@ import os
 
 # CONFIG
 ## URL of Spacewalk XML RPC server
-SATELLITE_URL = "http://SPACEWALK-SRV/rpc/api"
+SPACEWALK_URL = "http://SPACEWALK-SRV/rpc/api"
 ## User with Org Admin role (required to delete package in Spacewalk)
-SATELLITE_LOGIN = "XXXX"
-SATELLITE_PASSWORD = "YYYY"
+SPACEWALK_USERNAME = "XXXX"
+SPACEWALK_PASSWORD = "YYYY"
+
+# Set to 1 to see the XML and more
+VERBOSE_LEVEL = 0
 
 # Open connection to XML RPC server
-client = xmlrpclib.Server(SATELLITE_URL, verbose=0)
-key = client.auth.login(SATELLITE_LOGIN, SATELLITE_PASSWORD)
+client = xmlrpclib.Server(SPACEWALK_URL, verbose=VERBOSE_LEVEL)
+print('Attempting to connect to %s') % SPACEWALK_URL
+try:
+    key = client.auth.login(SPACEWALK_USERNAME, SPACEWALK_PASSWORD)
+    print 'Login succeeded'
+except xmlrpclib.Fault as e:
+    print 'Failed Login'
+    print "ERROR: Code:{0}.  String:{1}".format(err, strerr)
+    sys.exit()
+except socket.error as (err, strerr):
+    print('ERROR: Code:{0}.  String:{1}').format(err, strerr)
+    if err == 8:
+        print('Seems like your hostname is incorrect.')
+    sys.exit()
 
 # extract spacewalk channels
 list = client.channel.listAllChannels(key)
